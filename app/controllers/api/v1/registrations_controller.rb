34b68a7d060854fd5a34641c3d2967f2c0365259
Daffa Pradana <daffaarravi@gmail.com>
2025-10-26 12:15:23 +0700

    fix sessions and registrations error

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -15,7 +15,13 @@ module Api
       private
 
       def sign_up_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
+        params.require(:user).permit(
+          :email,
+          :password,
+          :password_confirmation,
+          :first_name,
+          :last_name,
+        )
       end
     end
   end
