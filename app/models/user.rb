754e5a899d73b4ae28fd7e31e51a8c6bb6d5a8c5
Daffa Pradana <daffaarravi@gmail.com>
2025-11-01 16:35:43 +0700

    fix rails helper configs and init user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :first_name, :last_name, presence: true
 end
